50eb5e9e21e950d41715bcaaca4ab5f37515fb97
Drew <dlederman69@gmail.com>
2022-08-29 22:07:46 -0600

    added init functionality

--- index.js
+++ index.js
@@ -12,104 +12,137 @@ const joinsPaths = path.join(resolvesPaths, "myteam.html");
 const render = require("./src/html.js");
 
 function init() {
-    function createMngr() {
-        inquirer.prompt([{
+  function createMngr() {
+    inquirer
+      .prompt([
+        {
+          type: "name",
+          message: "what is your managers name",
+          name: "name",
+        },
+        {
+          type: "id",
+          message: "what is your managers id",
+          name: "id",
+        },
+        {
+          type: "email",
+          message: "what is your managers email",
+          name: "email",
+        },
+        {
+          type: "officeNumber",
+          message: "what is your office number?",
+          name: "officeNumber",
 
-        }]);
-        createMenu();
-    }
-    function createMenu() {
-        // do you want to add an engineer? add questions.
-    }
-    function createEngineer(params) {
-        createMenu();
-    }
+          // ! what is yourt mnger name
+          // ! mangers is?
+          // ! mangers email?
+          // ! office number
+        },
+      ])
+      .then((val) => {
+        const manager = new Manager(val.name, val.id, val.email, val.officeNumber);
+        console.log(manager)   
+        //! manager card creation goes here.
+        createMenu();   
+     });
 
-    function createIntern(params) { }
-    function buildTeam() {
+   
+  }
+  function createMenu() {
+    // do you want to add an engineer? add questions. // ! what type of teamate do you want to add?
+    inquirer
+    .prompt([{
 
-    }
-    createMngr();
+    }])
+  }
+  function createEngineer(params) {
+    createMenu();
+  }
+
+  function createIntern(params) {}
+  function buildTeam() {}
+  createMngr();
 }
-
 
 init();
 
 // need to creaete a sort of generate markdown.
-const questions = [
-    {
-        type: "name",
-        message: "what is your managers name",
-        name: "title",
-    },
-    {
-        type: "input",
-        message: "enter intut here for first criteria",
-        name: "title",
-    },
-    {
-        type: "input",
-        message: "enter intut here for first criteria",
-        name: "title",
-    },
-    {
-        type: "input",
-        message: "enter intut here for first criteria",
-        name: "title",
-    },
-    {
-        type: "input",
-        message: "enter intut here for first criteria",
-        name: "title",
-    },
-    {
-        type: "input",
-        message: "enter intut here for first criteria",
-        name: "title",
-    },
-    {
-        type: "input",
-        message: "enter intut here for first criteria",
-        name: "title",
-    },
-    {
-        type: "input",
-        message: "enter intut here for first criteria",
-        name: "title",
-    },
-    {
-        type: "input",
-        message: "enter intut here for first criteria",
-        name: "title",
-    },
-    {
-        type: "input",
-        message: "enter intut here for first criteria",
-        name: "title",
-    },
-];
-//we will need some sort of validation
+// const questions = [
+//   {
+//     type: "name",
+//     message: "what is your managers name",
+//     name: "title",
+//   },
+//   {
+//     type: "input",
+//     message: "enter intut here for first criteria",
+//     name: "title",
+//   },
+//   {
+//     type: "input",
+//     message: "enter intut here for first criteria",
+//     name: "title",
+//   },
+//   {
+//     type: "input",
+//     message: "enter intut here for first criteria",
+//     name: "title",
+//   },
+//   {
+//     type: "input",
+//     message: "enter intut here for first criteria",
+//     name: "title",
+//   },
+//   {
+//     type: "input",
+//     message: "enter intut here for first criteria",
+//     name: "title",
+//   },
+//   {
+//     type: "input",
+//     message: "enter intut here for first criteria",
+//     name: "title",
+//   },
+//   {
+//     type: "input",
+//     message: "enter intut here for first criteria",
+//     name: "title",
+//   },
+//   {
+//     type: "input",
+//     message: "enter intut here for first criteria",
+//     name: "title",
+//   },
+//   {
+//     type: "input",
+//     message: "enter intut here for first criteria",
+//     name: "title",
+//   },
+// ];
+// //we will need some sort of validation
 
-//!: write read me
-function writeToFile(fileName, data) {
-    return fs.writeFile(fileName, data, function (err) {
-        console.log(fileName);
-        console.log(data);
-        if (err) {
-            return console.log(err);
-        } else {
-            console.log("completed");
-        }
-    });
-}
+// //!: write read me
+// function writeToFile(fileName, data) {
+//   return fs.writeFile(fileName, data, function (err) {
+//     console.log(fileName);
+//     console.log(data);
+//     if (err) {
+//       return console.log(err);
+//     } else {
+//       console.log("completed");
+//     }
+//   });
+// }
 
-// !: init program
-function init() {
-    inquirer.prompt(questions).then(function (data) {
-        writeToFile("index.html", data);
-        console.log(data);
-    });
-}
+// // !: init program
+// function init() {
+//   inquirer.prompt(questions).then(function (data) {
+//     writeToFile("index.html", data);
+//     console.log(data);
+//   });
+// }
 
-//!: calling init program
-init();
+// //!: calling init program
+// init();
