0949b3b8f9a1bd11807c96a7114a3ab295cea9e4
Drew <dlederman69@gmail.com>
2022-08-29 18:41:04 -0600

    went over with tutor

--- index.js
+++ index.js
@@ -1,73 +1,114 @@
 // inquier
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 // fs mod to create html file simlar to the read activitie
+const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
+const Engineer = require("./lib/Engineer");
+const fs = require("fs");
+const path = require("path");
 
-// need to creaete a sort of generate markdown. 
-const questions = [{
-    type:'input',
-    message: 'enter intut here for first criteria',
-    name: 'title',
-}, {
-    type:'input',
-    message: 'enter intut here for first criteria',
-    name: 'title',
-}, {
-    type:'input',
-    message: 'enter intut here for first criteria',
-    name: 'title',
-}, {
-    type:'input',
-    message: 'enter intut here for first criteria',
-    name: 'title',
-}, {
-    type:'input',
-    message: 'enter intut here for first criteria',
-    name: 'title',
-}, {
-    type:'input',
-    message: 'enter intut here for first criteria',
-    name: 'title',
-}, {
-    type:'input',
-    message: 'enter intut here for first criteria',
-    name: 'title',
-}, {
-    type:'input',
-    message: 'enter intut here for first criteria',
-    name: 'title',
-}, {
-    type:'input',
-    message: 'enter intut here for first criteria',
-    name: 'title',
-}, {
-    type:'input',
-    message: 'enter intut here for first criteria',
-    name: 'title',
-}, ]
-//we will need some sort of validation
- 
-//!: write read me
-function writeToFile(fileName, data) {
-    return fs.writeFile(fileName, data, function(err) {
-        console.log(fileName)
-        console.log(data)
-        if (err) {
-            return console.log(err)
-        } else {
-            console.log('completed')
-        }
-    })
+const resolvesPaths = path.resolve(__dirname, "output");
+const joinsPaths = path.join(resolvesPaths, "myteam.html");
+const render = require("./src/html.js");
+
+function init() {
+    function createMngr() {
+        inquirer.prompt([{
+
+        }]);
+        createMenu();
+    }
+    function createMenu() {
+        // do you want to add an engineer? add questions.
+    }
+    function createEngineer(params) {
+        createMenu();
+    }
+
+    function createIntern(params) { }
+    function buildTeam() {
+
+    }
+    createMngr();
 }
 
 
+init();
+
+// need to creaete a sort of generate markdown.
+const questions = [
+    {
+        type: "name",
+        message: "what is your managers name",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "enter intut here for first criteria",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "enter intut here for first criteria",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "enter intut here for first criteria",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "enter intut here for first criteria",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "enter intut here for first criteria",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "enter intut here for first criteria",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "enter intut here for first criteria",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "enter intut here for first criteria",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "enter intut here for first criteria",
+        name: "title",
+    },
+];
+//we will need some sort of validation
+
+//!: write read me
+function writeToFile(fileName, data) {
+    return fs.writeFile(fileName, data, function (err) {
+        console.log(fileName);
+        console.log(data);
+        if (err) {
+            return console.log(err);
+        } else {
+            console.log("completed");
+        }
+    });
+}
 
 // !: init program
 function init() {
-    inquirer.prompt(questions)
-    .then(function(data){
-        writeToFile("index.html",(data));
-        console.log(data)
-    })
+    inquirer.prompt(questions).then(function (data) {
+        writeToFile("index.html", data);
+        console.log(data);
+    });
 }
 
 //!: calling init program
