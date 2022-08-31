22f3ee131e0b203e5280afd0ac965404cbe7842e
Drew <dlederman69@gmail.com>
2022-08-30 18:11:35 -0600

    adding functionality html document almost made

--- index.js
+++ index.js
@@ -10,60 +10,138 @@ const path = require("path");
 const resolvesPaths = path.resolve(__dirname, "output");
 const joinsPaths = path.join(resolvesPaths, "myteam.html");
 const render = require("./src/html.js");
+const generatehtml = require('./generatehtml');
 
 function init() {
-  function createMngr() {
-    inquirer
-      .prompt([
-        {
-          type: "name",
-          message: "what is your managers name",
-          name: "name",
-        },
-        {
-          type: "id",
-          message: "what is your managers id",
-          name: "id",
-        },
-        {
-          type: "email",
-          message: "what is your managers email",
-          name: "email",
-        },
-        {
-          type: "officeNumber",
-          message: "what is your office number?",
-          name: "officeNumber",
+    const team = ['']
+    function createMngr() {
+        inquirer
+            .prompt([
+                {
+                    type: "name",
+                    message: "what is your managers name?",
+                    name: "name",
+                },
+                {
+                    type: "id",
+                    message: "what is your managers id?",
+                    name: "id",
+                },
+                {
+                    type: "email",
+                    message: "what is your managers email?",
+                    name: "email",
+                },
+                {
+                    type: "officeNumber",
+                    message: "what is your office number?",
+                    name: "officeNumber",
+                },
+            ])
+            .then((val) => {
+                const manager = new Manager(val.name, val.id, val.email, val.officeNumber);
+                team.push(manager);
+                //! manager card creation goes here.
+                createMenu();
+            });
+    }
 
-          // ! what is yourt mnger name
-          // ! mangers is?
-          // ! mangers email?
-          // ! office number
-        },
-      ])
-      .then((val) => {
-        const manager = new Manager(val.name, val.id, val.email, val.officeNumber);
-        console.log(manager)   
-        //! manager card creation goes here.
-        createMenu();   
-     });
 
-   
-  }
-  function createMenu() {
+function createMenu() {
     // do you want to add an engineer? add questions. // ! what type of teamate do you want to add?
     inquirer
-    .prompt([{
+        .prompt([{
+            type: "list",
+            message: "what type of teammate do you want to add?",
+            name: "addEmployeePrompt",
+            choices: ["Engineer", "Intern", "No more teammates needed"]
+        }])
 
-    }])
-  }
-  function createEngineer(params) {
-    createMenu();
-  }
+        .then(function (userInput) {
+            switch (userInput.addEmployeePrompt) {
+                case "Manager":
+                    addManager();
+                    break;
+                case "Engineer":
+                    addEngineer();
+                    break;
+                case "Intern":
+                    addIntern();
+                    break;
 
-  function createIntern(params) {}
-  function buildTeam() {}
-  createMngr();
+                default:
+                    htmlBuilder();
+            }
+        })
+}
+function addEngineer() {
+    inquirer.prompt([
+
+        {
+            type: "input",
+            name: "engineerName",
+            message: "What is the engineer's name?"
+        },
+
+        {
+            type: "input",
+            name: "engineerId",
+            message: "What is the engineer's employee ID number?"
+        },
+
+        {
+            type: "input",
+            name: "engineerEmail",
+            message: "What is the engineer's email address?"
+        },
+
+        {
+            type: "input",
+            name: "engineerGithub",
+            message: "What is the engineer's GitHub username?",
+        }])
+        .then((val) => {
+            const engineer = new Engineer(val.engineerName, val.engineerId, val.engineerEmail, val.engineerGithub);
+            console.log(engineer)
+            //! manager card creation goes here.
+            createMenu();
+
+        })
+    }
+    function addIntern() {
+        inquirer.prompt([
+    
+            {
+                type: "input",
+                name: "internName",
+                message: "What is the Interns's name?"
+            },
+    
+            {
+                type: "input",
+                name: "internId",
+                message: "What is the intern's employee ID number?"
+            },
+    
+            {
+                type: "input",
+                name: "internEmail",
+                message: "What is the intern's email address?"
+            },
+    
+            {
+                type: "input",
+                name: "internSchool",
+                message: "What is the intern's GitHub username?",
+            }])
+            .then((val) => {
+                const intern = new Intern(val.internName, val.internId, val.internEmail, val.internSchool);
+                console.log(intern)
+                //! manager card creation goes here.
+                createMenu();
+            })
+        }
+createMngr();
 }
 
 init();
